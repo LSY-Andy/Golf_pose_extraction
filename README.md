8fb52e408abaf8635b19643ee236360ca1f89c4b
Hu Tianrun <tianrunhu@gmail.com>
2023-04-15 12:09:32 +0800

    loader

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 ## Dependency
 Python == 3.10
+
+## Algo
+keypoints:
+
+![keypoints](images/keypoints.png)
+
+## TODO list
+- [x] demo develop for pose estimation
+- [ ] demo develop for matching
--- src/pose_est/pose_extraction.py
+++ src/pose_est/pose_extraction.py
@@ -6,19 +6,26 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-# extract the poses from the video
-class PoseExtracter():
-    def __init__(self, name: str) -> List:
+class PoseExtractor():
+    '''
+    An extractor to read from a source and get all the pose estimations
+    '''
+
+    def __init__(self, name: str = '') -> None:
+        self.set_name(name)
+
+    def set_name(self, name: str) -> None:
         # TODO input data type checking
         self.name = name
-  
+
     def video_extract(self):
         # For webcam input:
+        # TODO: check the validation of the path
         cap = cv2.VideoCapture(self.name)
         landmarks = []
         with mp_pose.Pose(
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5) as pose:
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as pose:
             while cap.isOpened():
                 success, image = cap.read()
                 if not success:
@@ -50,14 +57,15 @@ class PoseExtracter():
 
     # TODO this image extraction is not tested yet. test it
     def image_extract(self) -> None:
-      # For static images:
+        # For static images:
+        # TODO: check the validation of the image file paths
         IMAGE_FILES = []
-        BG_COLOR = (192, 192, 192) # gray
+        BG_COLOR = (192, 192, 192)  # gray
         with mp_pose.Pose(
-            static_image_mode=True,
-            model_complexity=2,
-            enable_segmentation=True,
-            min_detection_confidence=0.5) as pose:
+                static_image_mode=True,
+                model_complexity=2,
+                enable_segmentation=True,
+                min_detection_confidence=0.5) as pose:
             for idx, file in enumerate(IMAGE_FILES):
                 image = cv2.imread(file)
                 image_height, image_width, _ = image.shape
@@ -76,26 +84,27 @@ class PoseExtracter():
                 # Draw segmentation on the image.
                 # To improve segmentation around boundaries, consider applying a joint
                 # bilateral filter to "results.segmentation_mask" with "image".
-                condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+                condition = np.stack(
+                    (results.segmentation_mask,) * 3, axis=-1) > 0.1
                 bg_image = np.zeros(image.shape, dtype=np.uint8)
                 bg_image[:] = BG_COLOR
-                annotated_image = np.where(condition, annotated_image, bg_image)
+                annotated_image = np.where(
+                    condition, annotated_image, bg_image)
                 # Draw pose landmarks on the image.
                 mp_drawing.draw_landmarks(
                     annotated_image,
                     results.pose_landmarks,
                     mp_pose.POSE_CONNECTIONS,
                     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-                cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+                cv2.imwrite('/tmp/annotated_image' +
+                            str(idx) + '.png', annotated_image)
                 # Plot pose world landmarks.
                 mp_drawing.plot_landmarks(
                     results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
 
-def main():
-    pose_extract = PoseExtracter('../../data/standard/standard.mp4')
-    results = pose_extract.video_extract()
-    # print(results.pose_landmarks)
-    print(results[0].pose_world_landmarks)
-
-if __name__ == '__main__':
-    main()
+    def extract(self):
+        ext = self.name.split('.')[-1]
+        if ext == 'mp4':
+            self.video_extract()
+        else:
+            self.image_extract()
