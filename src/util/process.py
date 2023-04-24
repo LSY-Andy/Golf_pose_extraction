79b8b2a9c6ed2aad83d22cf9401c3bd0fffa358e
Jing-yilin <1154896650@qq.com>
2023-04-24 16:48:49 +0800

    refactor: remove '__main__'

--- src/util/process.py
+++ src/util/process.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import cv2
 
@@ -41,12 +40,4 @@ def extract_keyframe(video_path, save_path, frame_interval=0.5):
     cap.release()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract keyframes from a video.')
-    parser.add_argument('--video_path', type=str, help='Path to the video file')
-    parser.add_argument('--save_path', type=str, help='Directory to save extracted keyframes')
-    parser.add_argument('--frame_interval', type=float, default=0.5, help='Interval between extracted frames (seconds)')
 
-    args = parser.parse_args()
-
-    extract_keyframe(args.video_path, args.save_path, args.frame_interval)
